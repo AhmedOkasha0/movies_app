f12f8ecdf4ff45255c7b74a944041322d2954887
ahmed okasha <ao1922302@gmail.com>
2023-08-05 15:29:52 +0300

    finished remote data sourec and contract class

--- lib/core/utils/app_constant.dart
+++ lib/core/utils/app_constant.dart
@@ -1,4 +1,2 @@
 class AppConstant {
-  static const String baseUrl = 'https://api.themoviedb.org/3';
-  static const String apiKey = '3f0feda1306a4d38e9cfc9586b1e7055';
 }
--- lib/movies/data/data_sourses/movie_remot_data_source.dart
+++ lib/movies/data/data_sourses/movie_remot_data_source.dart
@@ -1,13 +1,42 @@
 import 'package:dio/dio.dart';
 import 'package:movies_app/core/error/exceptions.dart';
+import 'package:movies_app/core/network/api_constance.dart';
 import 'package:movies_app/core/network/error_message_model.dart';
-import 'package:movies_app/core/utils/app_constant.dart';
 import 'package:movies_app/movies/data/models/movies_model.dart';
 
-class MovieRemoteDataSource {
+abstract class BaseMovieRemoteDataSource {
+  Future<List<MoviesModel>> getNowPlayingMovies();
+  Future<List<MoviesModel>> getPopularMovies();
+  Future<List<MoviesModel>> getTopRatedMovies();
+}
+
+class MovieRemoteDataSource extends BaseMovieRemoteDataSource {
+  @override
   Future<List<MoviesModel>> getNowPlayingMovies() async {
-    final response = await Dio().get(AppConstant.baseUrl + AppConstant.apiKey);
+    final response = await Dio().get(ApiConstance.nowPlayingEndpoint);
     if (response.statusCode == 200) {
+      return response.data;
+    } else {
+      throw ServerExeption(
+          errorMessageModel: ErrorMessageModel.fromjson(response.data));
+    }
+  }
+
+  @override
+  Future<List<MoviesModel>> getPopularMovies() async {
+    final response = await Dio().get(ApiConstance.nowPlayingEndpoint);
+    if (response.data == 200) {
+      return response.data;
+    } else {
+      throw ServerExeption(
+          errorMessageModel: ErrorMessageModel.fromjson(response.data));
+    }
+  }
+
+  @override
+  Future<List<MoviesModel>> getTopRatedMovies() async {
+    final response = await Dio().get(ApiConstance.topRatedMoviesEndpoint);
+    if (response.data == 200) {
       return response.data;
     } else {
       throw ServerExeption(
